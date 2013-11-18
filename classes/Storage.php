32aad7de4af18262abcdcb249817a1f1fdb9a1ad
Alessandro Frangioni <alessandro@gruppoinsieme.it>
2013-11-18 16:56:57 +0100

    removed trailing spaces

--- classes/Storage.php
+++ classes/Storage.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
  * Storage Module
- * 
+ *
  * @package		Storage
  * @category	Base
  * @author		Micheal Morgan <micheal@morgan.ly>
